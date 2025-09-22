6f0ff8af1c8d172abde74ea0f73ac11d2e707c71
hariharen9 <hurryharen@gmail.com>
2025-09-22 21:24:57 +0530

    minor fix

--- src/components/Login/LoginPage.tsx
+++ src/components/Login/LoginPage.tsx
@@ -22,11 +22,13 @@ const useIsMobile = (breakpoint = 768) => {
   const [isMobile, setIsMobile] = useState(window.innerWidth < breakpoint);
 
   useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < breakpoint);
-    };
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    const mediaQuery = window.matchMedia(`(max-width: ${breakpoint}px)`);
+    const handleResize = () => setIsMobile(mediaQuery.matches);
+
+    handleResize(); // Set initial value
+    mediaQuery.addEventListener('change', handleResize);
+
+    return () => mediaQuery.removeEventListener('change', handleResize);
   }, [breakpoint]);
 
   return isMobile;
